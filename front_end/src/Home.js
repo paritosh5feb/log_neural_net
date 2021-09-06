14dceff9e23ef2908d17930aef32d9f20b5a2c50
paritosh5feb <paritosh.rakesh.ddn@gmail.com>
2021-09-06 15:20:13 +0530

    Modified Form and Added training data

--- front_end/src/Home.js
+++ front_end/src/Home.js
@@ -14,27 +14,117 @@ export default Home;
 class JsonForm extends Component {
   constructor(props) {
     super(props);
-    this.state = { pre_view_data: "YES" };
+    this.state = {
+      CAL: 0,
+      CNC: 0,
+      GR: 0,
+      HRD: 0,
+      HRM: 0,
+      PE: 0,
+      ZDEN: 0,
+      DTC: 0,
+      DTS: 0
+    };
+    this.loadAndPreview = this.loadAndPreview.bind(this);
+    this.predictLog = this.predictLog.bind(this);
+    this.predictInBacth = this.predictInBacth.bind(this);
   }
-  handleEvent(event) {
+  loadAndPreview(event) {
     console.log(event.target.name + " " + event.target.value);
     //this.setState();
   }
+
+  predictLog(event) {
+    console.log(event.target.name + " " + event.target.value);
+    //this.setState();
+  }
+
+  predictInBacth(event) {}
   render() {
     return (
       <div>
         <form>
-          <input type="radio" name="pre_view_data" id="rb1" value="YES"></input>
-          <label for="rb1">YES</label>
-          <br />
-          <input type="radio" name="pre_view_data" id="rb2" value="NO"></input>
-          <label for="rb1">NO</label>
-          <br />
+          <table>
+            <tbody>
+              <tr>
+                <td>CAL</td>
+                <td>
+                  <input
+                    type="text"
+                    value={this.state.CAL}
+                    onChange={this.handleChange}
+                  ></input>
+                </td>
+              </tr>
+              <tr>
+                <td>CNC</td>
+                <td>
+                  <input
+                    type="text"
+                    value={this.state.CNC}
+                    onChange={this.handleChange}
+                  ></input>
+                </td>
+              </tr>
+              <tr>
+                <td>GR</td>
+                <td>
+                  <input
+                    type="text"
+                    value={this.state.GR}
+                    onChange={this.handleChange}
+                  ></input>
+                </td>
+              </tr>
+              <tr>
+                <td>HRD</td>
+                <td>
+                  <input
+                    type="text"
+                    value={this.state.HRD}
+                    onChange={this.handleChange}
+                  ></input>
+                </td>
+              </tr>
+              <tr>
+                <td>HRM</td>
+                <td>
+                  <input
+                    type="text"
+                    value={this.state.HRM}
+                    onChange={this.handleChange}
+                  ></input>
+                </td>
+              </tr>
+              <tr>
+                <td>PE</td>
+                <td>
+                  <input
+                    type="text"
+                    value={this.state.PE}
+                    onChange={this.handleChange}
+                  ></input>
+                </td>
+              </tr>
+              <tr>
+                <td>ZDEN</td>
+                <td>
+                  <input
+                    type="text"
+                    value={this.state.ZDEN}
+                    onChange={this.handleChange}
+                  ></input>
+                </td>
+              </tr>
+            </tbody>
+          </table>
           <input
             type="button"
-            value="Pre-view-data"
-            onClick={this.handleEvent}
+            value="Load Data and Pre-view"
+            onClick={this.loadAndPreview}
           ></input>
+          <input type="button" value="Predict"></input>
+          <input type="button" value="Predict in a Batch"></input>
         </form>
       </div>
     );
