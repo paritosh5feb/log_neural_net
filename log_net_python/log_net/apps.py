d38809a6273cda900e74781f589a6f0ebc5a4f31
paritosh5feb <paritosh.rakesh.ddn@gmail.com>
2021-09-07 15:50:57 +0530

    Added model loading code

--- log_net_python/log_net/apps.py
+++ log_net_python/log_net/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 import html
 import pathlib
 import os
+from tensorflow import keras
 
 
 
@@ -9,4 +10,5 @@ class LogNetConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'log_net'
     MODEL_PATH = Path("model")
-    
+    predictor = keras.models.load_model(MODEL_PATH
+    '/'+'model_sonic_logs')
